16c9a6b1cfcbccfd5e1c3a2a2130feb347d652ac
bushramahin <101120359+bushramahin@users.noreply.github.com>
2023-09-20 20:52:06 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo_list
+ Simple Cli todo list
---
+++ dist/todo.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let arr = [];
+let todo = [
+    {
+        name: "todo_list",
+        type: "string",
+        message: "Enter Item"
+    }
+];
+let more = [
+    {
+        name: "moreItems",
+        type: "confirm",
+        message: "Do you want to add more Items?"
+    }
+];
+let condition = true;
+while (condition) {
+    let { todo_list } = await inquirer.prompt(todo);
+    arr.push(todo_list);
+    let { moreItems } = await inquirer.prompt(more);
+    if (moreItems == true) {
+        condition = true;
+    }
+    else {
+        condition = false;
+        console.log(arr);
+    }
+}
+// for(let i = 0 ; i<=10 ; i++){
+//     let{todo_list} = await inquirer.prompt(todo)
+//     arr.push(todo_list)
+// }
+// console.log("Here is your list",arr);
